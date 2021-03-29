56b63d19582cf84540fd79d58ef35ff596a17303
aborn <loveaborn@foxmail.com>
2021-03-29 20:59:06 +0800

    我的&设置

--- pages/about/home.js
+++ pages/about/home.js
@@ -6,15 +6,39 @@ Component({
     token: '',
     id: '',
     desc: '',
+    arrow: 'arrow-down',
+    arrowShow: false,
+    activeNames: ['0'],
     typemap: {
-      "NORMAL" : "普通帐号",
-      "VIP" : "VIP帐号",
-      "TEST" : "测试帐号"
+      "NORMAL": "普通帐号",
+      "VIP": "VIP帐号",
+      "TEST": "测试帐号"
     }
   },
-  attached() {
-  },
+  attached() {},
   methods: {
+    onChange(event) {
+      this.setData({
+        activeNames: event.detail,
+      });
+    },
+    onShowDetail(e) {
+      console.log("show detail", e)
+      const {
+        name
+      } = e.currentTarget.dataset
+      if (name === "arrow-down") {
+        this.setData({
+          arrow: "arrow-up",
+          arrowShow: true
+        })
+      } else {
+        this.setData({
+          arrow: "arrow-down",
+          arrowShow: false
+        })
+      }
+    },
     updateConfigInfo() {
       var id = app.globalData.config.id;
       var token = app.globalData.config.token;
@@ -25,7 +49,13 @@ Component({
       if (id === testConfig.id && token === testConfig.token) {
         desc = "当前账号为测试账号，请设置自己的账号！";
       }
-      this.setData({id, token, desc, regDate, type})
+      this.setData({
+        id,
+        token,
+        desc,
+        regDate,
+        type
+      })
     }
   },
   lifetimes: {
