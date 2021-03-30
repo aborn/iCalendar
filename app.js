21f261413fd146762347747c776d18f5eea2de25
aborn <loveaborn@foxmail.com>
2021-03-30 12:02:09 +0800

    delete custom-tab-bar folder

--- app.js
+++ app.js
@@ -4,19 +4,6 @@ App({
     // 展示本地存储能力
     var config = wx.getStorageSync('config') || this.globalData.TestConfig
     this.globalData.config = config;
-
-    wx.getSystemInfo({
-      success: e => {
-        this.globalData.StatusBar = e.statusBarHeight;
-        let capsule = wx.getMenuButtonBoundingClientRect();
-        if (capsule) {
-          this.globalData.Custom = capsule;
-          this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
-        } else {
-          this.globalData.CustomBar = e.statusBarHeight + 50;
-        }
-      }
-    })
   },
   getToken() {
     return this.globalData.config.token;
