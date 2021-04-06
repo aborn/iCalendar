0618ff699413a956adf4cecc0111736a27f456ee
aborn <loveaborn@foxmail.com>
2021-04-07 07:15:34 +0800

    applyAccount

--- pages/about/home.js
+++ pages/about/home.js
@@ -14,7 +14,25 @@ Component({
       "NORMAL": "普通帐号",
       "VIP": "VIP帐号",
       "TEST": "测试帐号"
-    }
+    },
+    show: false,
+    isTest: false,
+    token: '',
+    id: '',
+    ctoken: '',
+    cid: '',
+    beforeClose: (action) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          if (action === 'confirm') {
+            resolve(true);
+          } else {
+            // 拦截取消操作
+            resolve(false);
+          }
+        }, 1000);
+      });
+    },
   },
   attached() {},
   methods: {
@@ -49,6 +67,9 @@ Component({
       var testConfig = app.globalData.TestConfig;
       if (id === testConfig.id && token === testConfig.token) {
         desc = "当前为测试帐号，请设置自己的帐号！";
+        this.setData({
+          isTest: true
+        })
       }
       this.setData({
         id,
@@ -57,7 +78,46 @@ Component({
         regDate,
         type
       })
-    }
+    },
+    applyAccount(e) {
+      this.setData({
+        show: true
+      })
+    },
+    onChange(e) {
+      var cvalue = e.detail;
+      const {
+        field,
+        value
+      } = e.currentTarget.dataset
+      console.log('当前值为：' + cvalue + ", field=" + field + ", value=" + value)
+
+      if (field === 'id') {
+        this.setData({
+          cid: cvalue
+        })
+      } else if (field === 'token') {
+        this.setData({
+          ctoken: cvalue
+        })
+      }
+    },
+    onSubmit(event) {
+      var that = this;
+      var token = this.data.token;
+      var id = this.data.id;
+      var ctoken = this.data.ctoken;
+      var cid = this.data.cid;
+
+      console.log('token=' + token + ', id=' + id +
+        ', 当前表单值：ctoken=' + ctoken + ', cid=' + cid)
+      console.log(event.detail);
+    },
+    onClose() {
+      this.setData({
+        show: false
+      });
+    },
   },
   lifetimes: {
     ready() {
